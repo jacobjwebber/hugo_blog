574eee9fbb794defd98789c1d07c95a154932060
Jacob Webber <s1116548@twite.inf.ed.ac.uk>
2017-04-01 18:03:14 +0100

    Adding automated build - could ruin everything

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo building and deploying blog
+set -e
+hugo --theme=hugo-theme-nix
+cd ..
+git clone git@github.com:jacobjwebber/jacobjwebber.github.io.git
+cp -r hugo_blog/public jacobjwebber.github.io/
+cd jacobjwebber.github.io
+git commit -am "automated deploy $(date)"
+git push origin master
