a877f7e1866c90159d53ab3568e331dd3d7e448e
Jacob Webber <s1116548@twite.inf.ed.ac.uk>
2017-04-01 18:14:48 +0100

    Adding automated build - could ruin everything https

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo building and deploying blog
 set -e
 hugo --theme=hugo-theme-nix
 cd ..
-git clone git@github.com:jacobjwebber/jacobjwebber.github.io.git
+git clone https://github.com/jacobjwebber/jacobjwebber.github.io.git
 cp -r hugo_blog/public jacobjwebber.github.io/
 cd jacobjwebber.github.io
 git commit -am "automated deploy $(date)"
